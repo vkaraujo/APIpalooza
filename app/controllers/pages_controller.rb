a66b582800741180518f9a571a02f3e162820dba
Viktor Araujo <vkaraujo00@gmail.com>
2020-06-25 20:15:12 -0300

    Trello autofill implemented

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,42 +1,39 @@
 class PagesController < ApplicationController
+  require 'json'
+  require 'net/http'
+  require 'open-uri'
+  
   def home
+    if params[:query].present?
+      
+
+      name = params[:query]
+      @url = "https://api.openweathermap.org/data/2.5/weather?q=#{name.to_s}&appid=6665c7eaa20bb85d40242ced15d50d83"
+      @uri = URI(@url)
+      @response = Net::HTTP.get(@uri)
+      @output = JSON.parse(@response)
+
+      if @output.empty?
+        @final_output = {"weather"=>[{"main"=>"N/A"}], "main"=>[{"temp"=>273, "humidity"=>"N/A"}] }
+      elsif !@output
+        @final_output = {"weather"=>[{"main"=>"N/A"}], "main"=>[{"temp"=>273, "humidity"=>"N/A"}] }
+      else
+        @final_output = @output
+      end
+    end  
   end
 
-  def search
-    cities = find_city(params[:city])
-    unless countries
-      flash[:alert] = 'City not found'
-      return render action: :home
-    end
+  def trello
+  end  
 
-     @city = cities.first
-     @weather = find_weather(@final_output['weather'][0]['main'])
+  def beer
+    @url = "https://sandbox-api.brewerydb.com/v2/beer/random/?key=76b511fd2a3a2643da15a48aae4c1bd0"
+    
 
   end  
 
   private
 
-  def request_api(url)
-    require 'open-uri'
-    require 'json'
-    require 'net/http'
-
-    @uri = URI(@url)
-    @response = Net::HTTP.get(@uri)
-    @output = JSON.parse(@response)
-
-    if @output.empty?
-      @final_output = "Error"
-    elsif !@output
-      @final_output = "Error"
-    else
-      @final_output = @output
-    end
-  end
-
-  def find_city(name)
-    request_api(
-      "https://api.openweathermap.org/data/2.5/weather?q=#{name}&appid=6665c7eaa20bb85d40242ced15d50d83"
-    )
-  end
+  def request_api
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  get '/search' => 'pages#search'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  get '/search' => 'pages#search'
+  get '/trello', to: 'pages#trello'
 end
