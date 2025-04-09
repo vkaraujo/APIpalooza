3de2b2a1449cb16f8d9ba8e6e04fdaef04a692ba
viktor <araujo>
2025-04-09 20:00:02 -0300

    Add YouTube API integration with embedded video grid and responsive layout

--- config/routes.rb
+++ config/routes.rb
@@ -8,8 +8,8 @@ Rails.application.routes.draw do
   resources :numbers, only: [:index, :create]
   resources :trivia, only: [:index, :create]
   post "/trivia/check", to: "trivia#check", as: :check_trivia
+  resources :youtube, only: [:index, :create]
 
   # Health check
   get "up" => "rails/health#show", as: :rails_health_check
 end
-
